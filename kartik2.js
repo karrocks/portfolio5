035ae873e06911c4d338a7dfd825af8f0019aae6
karrocks <8guptakartik@gmail.com>
2022-06-15 23:44:24 +0530

    intial commit

---
+++ kartik2.js
@@ -0,0 +1,24 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
+  document.body.classList.add('js-loading');
+
+  window.addEventListener("load", showPage);
+  
+  function showPage() {
+    document.body.classList.remove('js-loading');
+  }
