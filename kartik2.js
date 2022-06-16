4c533c8f77bb7ca8e482e623e50df0d1dd2f96e0
Kartik Gupta <74768115+karrocks@users.noreply.github.com>
2022-06-16 16:03:38 +0530

    Add files via upload

--- kartik2.js
+++ kartik2.js
@@ -1,24 +1,29 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
     }
   }
-  
-  window.addEventListener("scroll", reveal);
+}
+
+window.addEventListener("scroll", reveal);
 
 
-  document.body.classList.add('js-loading');
-
-  window.addEventListener("load", showPage);
-  
-  function showPage() {
-    document.body.classList.remove('js-loading');
-  }
+function startAnimation() {
+  document.querySelector('#firsttitlebg').style.animationPlayState = 'running';
+  document.querySelector('.firsttitle').style.animationPlayState = 'running';
+  document.querySelector('.button1anim').style.animationPlayState = 'running';
+  document.querySelector('.firstmore').style.animationPlayState = 'running';
+  document.querySelector('.animsecondimg').style.animationPlayState = 'running';
+  document.querySelector('.animsecondbg ').style.animationPlayState = 'running';
+  document.querySelector('.animsecondhead').style.animationPlayState = 'running';
+  document.querySelector('.button2anim').style.animationPlayState = 'running';
+  document.querySelector('.animsecondmore').style.animationPlayState = 'running';
+}
+window.addEventListener('load', startAnimation);
